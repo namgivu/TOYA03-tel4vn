401e978bd460546a175c71e50ca8dbe3437fc794
Nam G VU <namgivu@gmail.com>
2023-10-03 20:47:18 +0700

    GET /demo_requests

--- s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/README.md
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/README.md
@@ -8,6 +8,7 @@ python -m virtualenv .venv
 ./.venv/bin/pip install Flask
 ./.venv/bin/pip freeze
 
+
 --- api endpoint / and /hi
 FLASK_APP=src.app  ./.venv/bin/flask run
 
@@ -16,3 +17,8 @@ curl localhost:5000
 
 curl localhost:5000/hi
 > hi @ /hi
+
+
+--- install requests package @ GET /demo_requests
+./.venv/bin/pip install requests
+./.venv/bin/pip freeze
--- s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/src/app.py
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 import os
+import requests
+import json
+
 
 app = Flask(__name__)
 
@@ -20,11 +23,17 @@ GET /hi  ->  hi
     /hi route
 '''
 @app.route('/hi')
-def hi():
+def          hi():
   return 'hi @ /hi'
 
 
+@app.route('/demo_requests')
+def          demo_requests():
+  res =           requests.get('https://api.upcitemdb.com/prod/trial/lookup?upc=190199267961')
+  return json.dumps(res.json())
+
+
 if __name__ == '__main__':  #TODO explain later what is this line
-  app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000) )
+  app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000) )  #TODO param to autoreload code wh changed
   #                       port=run w/ custom port     3000 default port if envvar PORT not avail.
   #       host  0 0 0 0 to accept request fr anywhere
