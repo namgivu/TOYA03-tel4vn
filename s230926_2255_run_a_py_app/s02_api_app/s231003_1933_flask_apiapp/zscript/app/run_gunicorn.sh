039773bb65b2ffcd2ebb94dab288449c4d79bde8
Nam G VU <namgivu@gmail.com>
2023-10-03 21:03:23 +0700

    run w/ gunicorn w/ custom port

--- s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/zscript/app/run_gunicorn.sh
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/zscript/app/run_gunicorn.sh
@@ -3,5 +3,5 @@ AH=`cd "$SH/../.." && pwd`  # AH aka APP_HOME
 
 cd $AH
   # ref https://stackoverflow.com/a/41940807/248616
-  ./.venv/bin/gunicorn -b 0.0.0.0:'3000' src.app:app
-  #                    -b                       :app app=Flask() instance in module src.app
+  ./.venv/bin/gunicorn -b 0.0.0.0:${PORT:-3000}  src.app:app
+  #                    -b                               :app app=Flask() instance in module src.app
