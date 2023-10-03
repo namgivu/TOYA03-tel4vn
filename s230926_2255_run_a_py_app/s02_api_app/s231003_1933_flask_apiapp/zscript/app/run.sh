112b4ab343ca21bfdb1d92e31e209d858a9e74bd
Nam G VU <namgivu@gmail.com>
2023-10-03 20:25:09 +0700

    -p custom port

--- s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/zscript/app/run.sh
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/zscript/app/run.sh
@@ -2,4 +2,5 @@ SH=`cd $(dirname ${BASH_SOURCE:-$0}) && pwd`  # SH aka SCRIPT_HOME
 AH=`cd "$SH/../.." && pwd`  # AH aka APP_HOME
 
 cd $AH
-  FLASK_APP=src.app  ./.venv/bin/flask run
+  FLASK_APP='src.app' ./.venv/bin/flask run  -p${PORT:-5000}
+  #        =                      flask run  -p custom port  ref https://stackoverflow.com/a/41940807/248616
