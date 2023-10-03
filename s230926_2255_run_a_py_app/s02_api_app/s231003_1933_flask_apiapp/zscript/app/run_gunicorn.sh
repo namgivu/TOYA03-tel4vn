ec03319ffd9a478fbc18f527affbb31bc1c8b73f
Nam G VU <namgivu@gmail.com>
2023-10-03 21:02:21 +0700

    run w/ gunicorn

---
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1933_flask_apiapp/zscript/app/run_gunicorn.sh
@@ -0,0 +1,7 @@
+SH=`cd $(dirname ${BASH_SOURCE:-$0}) && pwd`  # SH aka SCRIPT_HOME
+AH=`cd "$SH/../.." && pwd`  # AH aka APP_HOME
+
+cd $AH
+  # ref https://stackoverflow.com/a/41940807/248616
+  ./.venv/bin/gunicorn -b 0.0.0.0:'3000' src.app:app
+  #                    -b                       :app app=Flask() instance in module src.app
