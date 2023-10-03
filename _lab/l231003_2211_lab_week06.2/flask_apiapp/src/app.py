de19584e75a3d7052485d768830e0ed2b38a0398
Nam G VU <namgivu@gmail.com>
2023-10-03 22:39:31 +0700

    call github api /tags to get latesttag

--- _lab/l231003_2211_lab_week06.2/flask_apiapp/src/app.py
+++ _lab/l231003_2211_lab_week06.2/flask_apiapp/src/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 import json
+#
+from dotenv import load_dotenv
+import os
+import requests
 
 
 app = Flask(__name__)
@@ -8,6 +12,36 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return json.dumps({})
+
+@app.route('/tag')
+def          tag():
+  '''
+  ?repo=<repo-name>  <-- request.args  ref https://stackoverflow.com/a/11774434/248616
+  '''
+  owner_reponame = request.args.get('repo')
+
+  #region    call github api /tags
+  load_dotenv()
+  GITHUB_API_KEY = os.environ.get('GITHUB_API_KEY')  # ref https://developers.google.com/maps/documentation/geocoding/get-api-key
+  headers = {
+    'Authorization': f'Bearer {GITHUB_API_KEY}',
+  }
+
+  owner_reponame =  'pyenv/pyenv'  # Nam G VU 's favourite public github repo
+  url            = f'https://api.github.com/repos/{owner_reponame}/tags'
+
+  res = requests.get(url, headers=headers)
+  d   = res.json()
+
+  latest_tag = d[0]['name']
+  #endregion call github api /tags
+
+  return json.dumps({
+    'latest_tag': latest_tag,
+  })
+
+#endregion call github api to list tags
+
 
 
 if __name__ == '__main__':
