2d999c80668e722a4a4edd5b8b020f820bf30cf0
Nam G VU <namgivu@gmail.com>
2023-10-19 22:07:16 +0700

    review githubapi @ requests.get(url, header)

--- s230921_2256_github_api/python_consoleapp/app.py
+++ s230921_2256_github_api/python_consoleapp/app.py
@@ -1,4 +1,22 @@
 import requests  # pip install requests <-- prepare .venv
 
-#              url                                header
-requests('todo url -> github api endpoint', 'todo header w/ github apikey')
+'''
+ref https://docs.github.com/en/rest/git/tags?apiVersion=2022-11-28  ref2 https://rapidapi.com/blog/directory/github-git-tags/#:~:text=The%20GitHub%20Git%20Tags%20API,to%20access%20to%20this%20API.
+https://api.github.com/repos/:owner/:repo/tags/
+https://api.github.com/repos/pyenv/pyenv/tags/
+'''
+
+#               url                                header
+# requests.get('url -> github api endpoint', 'todo header w/ github apikey')
+
+
+#      https://api.github.com/repos/:owner/:repo/git/tags/   WRONG
+#      https://api.github.com/repos/:owner/:repo/tags        CAUTION no / attheend
+url = 'https://api.github.com/repos/pyenv/pyenv/tags'
+
+
+GITHUB_API_KEY = 'yourapikey'
+header         = { 'Authorization': f'Bearer {GITHUB_API_KEY}' }
+res = requests.get(url, header)
+
+print(res.json())
