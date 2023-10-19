d215e7cd0dc12b35f50ec718998f7176570d4c48
Nam G VU <namgivu@gmail.com>
2023-10-19 22:51:10 +0700

    review flask apiapp @ GET /github/latesttag

--- s230926_2255_run_a_py_app/s02_api_app/s231003_1934_flask_apiapp/app.py
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1934_flask_apiapp/app.py
@@ -1,4 +1,5 @@
-from flask import Flask  # pip install flask
+from flask import Flask, jsonify  # pip install flask
+import requests  # pip install requests
 
 
 app = Flask(__name__)
@@ -6,6 +7,27 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return ''
+
+@app.route('/github/latesttag')
+def github_latesttag():
+  #      https://api.github.com/repos/:owner/:repo/tags        CAUTION no / attheend
+  url = 'https://api.github.com/repos/pyenv/pyenv/tags'
+
+  GITHUB_API_KEY = 'yourkey'
+  header         = { 'Authorization': f'Bearer {GITHUB_API_KEY}' }  #TODO it seems github now allow to call /tags endpoint without a github api key
+  res = requests.get(url, header)
+
+  d = res.json()
+  latest_tag = d[0] ['name']
+  return jsonify({'latest_tag': latest_tag})
+
+  '''
+  HTTP/1.1 200 OK
+  {
+      "latest_tag": "v20160726"
+  }
+  '''
+
 
 if __name__=='__main__':
   app.run()
--- s230926_2255_run_a_py_app/s02_api_app/s231003_1934_flask_apiapp/script/r01_install_pkg.sh
+++ s230926_2255_run_a_py_app/s02_api_app/s231003_1934_flask_apiapp/script/r01_install_pkg.sh
@@ -9,6 +9,6 @@ echo -e "\n--- before" ; ./.venv/bin/pip freeze
 # install :requests
 echo
 ./.venv/bin/python -m pip install --upgrade pip
-./.venv/bin/pip install flask
+./.venv/bin/pip install flask requests
 
 echo -e "\n--- after" ; ./.venv/bin/pip freeze
